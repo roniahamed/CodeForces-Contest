dc3a1f8de52ce1904279fd4d63bc1b4dcfe24880
Md Roni Ahamed <mdroni0193101@gmail.com>
2023-06-01 22:14:06 +0600

    update

---
+++ 219158D-Difference.cpp
@@ -0,0 +1,18 @@
+/*
+ **===================================**
+ **        MD Roni Ahamed             **
+ **            Phitron                **
+ **                                   **
+ **===================================**
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+    long long sum = 0;
+    sum = (a*b)-(c*d);
+    cout<<"Difference = "<<sum<<endl;
+    return 0;
+
+    }
---
+++ 219158E-Area of a Circle.cpp
@@ -0,0 +1,16 @@
+/*
+ **===================================**
+ **        MD Roni Ahamed             **
+ **            Phitron                **
+ **                                   **
+ **===================================**
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double a, b;
+    cin>>a;
+    b = 3.141592653;
+    double area = b * a*a;
+    cout<<fixed<<setprecision(9)<<area<<endl;
+}
