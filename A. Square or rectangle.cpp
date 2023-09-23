328bb46a1d4fe9d6318a3b585595cf8274ea3df6
MdRoniAhamed <mdroni0193101@gmail.com>
2023-09-23 22:25:30 +0600

    Solve

---
+++ A. Square or rectangle.cpp
@@ -0,0 +1,29 @@
+/*
+ **===================================**
+ **        AE Roni Ahamed             **
+ **            Phitron                **
+ **                                   **
+ **===================================**
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int w,h;
+        cin>>w>>h;
+        if(w==h)
+            cout<<"Square\n";
+        else
+            cout<<"Rectangle\n";
+    }
+
+    return 0;
+}
+
+
